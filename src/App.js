4ed885d538b09b7bc30c4ded8963858ec15c8d3a
aneesh072 <aneeshkarki292@gmail.com>
2022-08-02 19:46:39 +1000

    added dummy table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th> Name </th>
+            <th> Address</th>
+            <th> Phone Number </th>
+            <th> Email </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Henny Chen</td>
+            <td> 3 waterfoot road</td>
+            <td> 123 123 1234</td>
+            <td>j.channey@gmail.com</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
